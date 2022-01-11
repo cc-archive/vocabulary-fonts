2b0612f50537fc2989ac7c0c3b607afdb2f4bf9b
Brylie Christopher Oxley <brylie@creativecommons.org>
2022-01-11 13:01:25 +0200

    Use Prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     node: true
   },
   extends: [
-    'standard'
+    "standard",
+    "prettier"
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
